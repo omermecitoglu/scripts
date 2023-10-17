014818d905444feed04726c38bd767cee8b032ff
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2023-10-17 12:53:33 +0700

    Create unbind-arrow-combos.sh

---
+++ unbind-arrow-combos.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# unbind workspace arrow combos
+
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-right "[]"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-left "[]"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-up "[]"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-down "[]"
